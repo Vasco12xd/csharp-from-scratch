41dd339b6ff5a6701ee4fd6c7d5a113d963c38fb
vasco12xd <evasco.27@hotmail.com>
2026-03-23 15:22:16 -0500

    Add logic for conditional and operators

---
+++ conditionals_and_operators/Program.cs
@@ -0,0 +1,16 @@
+﻿// Conditionals
+ 
+/*Console.WriteLine("Ingrese el primer número: ");
+int num = int.Parse(Console.ReadLine());
+if (num > 0 ) Console.WriteLine("Positivo");
+else if ( num < 0 ) Console.WriteLine("Negativo");
+else Console.WriteLine("Es cero"); */
+
+
+//Operators
+Console.WriteLine("Ingrese el primer número: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el Segundo número: ");
+double b = double.Parse(Console.ReadLine());
+const string Indeterminado = "Indeterminado";
+Console.WriteLine($"Suma: {a+b}, resta: {a-b}, Multiplicado: {a*b}, División: {(b!=0 ? a/b : Indeterminado)}");
